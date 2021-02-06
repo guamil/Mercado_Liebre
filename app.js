3436f387562dd99f52c2deadba8c883052a15678
guamil <gustavo.j.amil@gmail.com>
2021-02-06 08:51:43 -0300

    subiendo primer presentacion lista mercado liebre

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use( express.static (publicPath) );
 
-app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
+app.listen(process.env.PORT || 3000, function*(){
+    console.log("Servidor corriendo en el puerto 3000");
+});
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html")) 
